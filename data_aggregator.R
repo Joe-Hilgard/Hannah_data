fd592edb5044fbc0bc03a75416f844927921cefc
Joe Hilgard <jhilgard@gmail.com>
2015-03-06 10:11:07 -0600

    Fixed Block3 fixation data. - Commented out View() commands.

--- data_aggregator.R
+++ data_aggregator.R
@@ -3,7 +3,7 @@
 # Read in the data file
 dat <- read.delim("./raw_data/Sub1.txt") 
 head(dat)
-View(dat)
+#View(dat)
 
 # Basic data import, label, and concatenate loop
 dat1 <- read.delim("./raw_data/Sub1.txt")
@@ -48,9 +48,9 @@ grep("Merc", row.names(mtcars), value=T)
 dat$race = NA
 # Where FaceFilename contains "Black", we set dat$race to "black"
 dat$race[grep("Black", dat$FaceFilename)] = "Black"
-View(dat)
+#View(dat)
 dat$race[grep("White", dat$FaceFilename)] = "White"
-View(dat)
+#View(dat)
 
 # We can do a similar thing to fix the point of fixation
 forehead_range = c(1:8)
@@ -67,9 +67,30 @@ for (i in eyes_range) {
 for (i in nose_range) {
   dat$fixation[grep(i, dat$FaceFilename)] = "nose"
 } # not a problem now because it won't match 21 to 1
-View(dat)
+#View(dat)
 sub2dat = dat[dat$ID == 2,]
 table(sub2dat$fixation, sub2dat$Block)
+
+# Fix Block 3 fixation data, too
+table(dat$FaceFilename[dat$Block==3]) # looks like range is 1-48
+
+# reuse code from above but change ranges
+forehead_range = c(1:16)
+eyes_range = c(17:32)
+nose_range = c(33:48)
+
+for (i in forehead_range) {
+  dat$fixation[grepl(i, dat$FaceFilename) & dat$Block == 3] = "forehead"
+} # note that this will still match 1 with 11, 21, but we're gonna fix those later
+for (i in eyes_range) {
+  dat$fixation[grepl(i, dat$FaceFilename) & dat$Block == 3] = "eyes"
+}
+for (i in nose_range) {
+  dat$fixation[grepl(i, dat$FaceFilename) & dat$Block == 3] = "nose"
+} # not a problem now because it won't match 21 to 1
+sub2dat = dat[dat$ID == 2,]
+table(sub2dat$fixation, sub2dat$Block)
+
 
 # export data for analysis
 write.table(dat, "cleaned_data.txt", sep="\t", row.names=F)
