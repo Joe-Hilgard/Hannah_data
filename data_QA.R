b1be59971dd86a843c857f7c53d2089f6c67aefd
Joe Hilgard <jhilgard@gmail.com>
2015-03-20 13:50:55 -0500

    Added stuff to quality-assurance.

--- data_QA.R
+++ data_QA.R
@@ -28,3 +28,49 @@ meanError
 barplot(meanError)
 hist(meanError, breaks=10)
 
+# I find that significance testing sometimes works well for
+  # subject inclusion/exclusion.
+# We want participants whose behavior would be weird (p<.01)
+  # if they were totally inaccurate.
+
+# What's the critical accuracy for p<.01?
+  # How many trials do we have / subject?
+dim(dat2[dat2$ID==2,]) # 192 rows / subject
+table(dat2$ID)
+table(dat2$fixation[dat2$ID==2]) # 64+64+64 = 192
+# Probability of getting trial correct by chance is 50%
+# so what's the critical number of correct trials to be 
+  # not an asshole, p<.01?
+alpha = .01
+qbinom(p = 1-alpha, 
+       size = 192,
+       prob=.5)
+# grab subjects who got <112 correct
+tab = 192 - tapply(dat2$Error, dat2$ID, FUN=sum)
+badTab = tab[tab < 112]
+# make a log of it
+badSubs2 = data.frame("ID" = names(badTab),
+                     "Block" = 2,
+                     "correctCount" = badTab,
+                     "reason" = "Bad accuracy")
+# Repeat for Block 3
+table(dat3$ID) # 96 trials/subject
+# grab subjects who got <112 correct
+tab = 192 - tapply(dat3$Error, dat3$ID, FUN=sum)
+badTab = tab[tab < 112]
+# critical number of correct?
+alpha = .01
+qbinom(p = 1-alpha, 
+       size = 96,
+       prob=.5) # 59
+# grab subjects who got <59 correct
+tab = 96 - tapply(dat3$Error, dat3$ID, FUN=sum)
+badTab = tab[tab < 59]
+# make a log of it
+badSubs3 = data.frame("ID" = names(badTab),
+                     "Block" = 3,
+                     "correctCount" = badTab,
+                     "reason" = "Bad accuracy")
+# combine the data tables
+badSubs = rbind(badSubs2, badSubs3)
+
